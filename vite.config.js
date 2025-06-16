d61d0135aaaa3aae84ccdc8fe71b620cb33ce0fc
OloToby <espoiradouwekonou20@gmail.com>
2025-06-16 13:49:23 +0200

    Suppression de 'dist' du .gitignore, ajout des fichiers de build et config Vite pour GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/espoir-adouwekonou/", // AJOUTE CETTE LIGNE
+  base: "/espoir-adouwekonou/", // Cette ligne est cruciale pour GitHub Pages
 })
